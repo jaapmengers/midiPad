abf1566ce65e66b7586bd4cb1c7a1a2a0c68382c
Jaap Mengers <jaapm@q42.nl>
2014-03-06 17:02:38 +0100

    Mock de midiservice om periodiek aan signaal af te geven

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express'),
   http = require('http'),
   path = require('path'),
-  midi = require('./node-midi/midi'),
+  // midi = require('./node-midi/midi'),
   us = require('underscore');
 
 
@@ -17,30 +17,34 @@ var port = process.env.PORT || 5000;
 server.listen(port);
 
 // Set up a new input.
-var input = new midi.input();
+// var input = new midi.input();
 
 // Configure a callback.
 var i = 0;
 var deltas = [];
-input.on('message', function(deltaTime, message) {
+// input.on('message', function(deltaTime, message) {
 
-		if(message[0] === 248){
-			deltas.push(deltaTime);
-			i++;
-			if(i === (24 * 2)){
-				var avgDeltas = us.reduce(deltas, function(memo, num){ return memo + num; }, 0) / deltas.length;
-				var avgDeltasMs = (1/avgDeltas) * 10;
+// 		if(message[0] === 248){
+// 			deltas.push(deltaTime);
+// 			i++;
+// 			if(i === (24 * 2)){
+// 				var avgDeltas = us.reduce(deltas, function(memo, num){ return memo + num; }, 0) / deltas.length;
+// 				var avgDeltasMs = (1/avgDeltas) * 10;
 				
-				io.sockets.emit("currentBeatDelta", {freq: avgDeltasMs});
-				i=0;
-				deltas = [];
-				console.log("Average", avgDeltasMs);
-			}
-		} else {
-			i =0;
-		}
-});
+// 				io.sockets.emit("currentBeatDelta", {freq: avgDeltasMs});
+// 				i=0;
+// 				deltas = [];
+// 				console.log("Average", avgDeltasMs);
+// 			}
+// 		} else {
+// 			i =0;
+// 		}
+// });
 
+setInterval(function(){
+	io.sockets.emit("currentBeatDelta", {freq: 1000});
+	console.log("Blalalala");
+}, 10000)
 
 io.sockets.on('connection', function(socket){
 
@@ -53,8 +57,8 @@ io.sockets.on('connection', function(socket){
 });
 
 // Create a virtual input port.
-input.openVirtualPort("midiPad");
+// input.openVirtualPort("midiPad");
 
-input.ignoreTypes(false, false, false);
+// input.ignoreTypes(false, false, false);
 
 //input.closePort();
